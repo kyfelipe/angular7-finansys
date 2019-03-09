63c27a0e1be3194dbc9b77faaf62ae19feaee054
ISA'- <contatos.cortes@gmail.com>
2019-03-09 17:00:55 -0300

    Adicionado a herança BaseResourceListComponent no CategoryListComponent

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,33 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Category } from '../shared/category.model';
 import { CategoryService } from '../shared/category.service';
+import { BaseResourceListComponent } from 'src/app/shared/components/base-resource-list/base-resource-list.component';
 
 @Component({
     selector: 'app-category-list',
     templateUrl: './category-list.component.html',
     styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent implements OnInit {
+export class CategoryListComponent extends BaseResourceListComponent<Category> {
 
-    categories: Category[] = [];
-
-    constructor(private categoryService: CategoryService) { }
-
-    ngOnInit() {
-        this.categoryService.getAll().subscribe(
-            (categories) => this.categories = categories,
-            (error) => console.log('Error ao carregar a lista de categorias', error)
-        );
+    constructor(protected categoryService: CategoryService) {
+        super(categoryService);
     }
 
-    public deleteCategory(category) {
-        const mustDelete = confirm('Deseja realmente deletar essa categoria?');
-        
-        if(mustDelete) {
-            this.categoryService.delete(category.id).subscribe(
-                () => this.categories = this.categories.filter((element) => element !== category),
-                (error) => console.log('Error ao tentar excluir a categoria: ', error)
-            );
-        }
-    }
 }
